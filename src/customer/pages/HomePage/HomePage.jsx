3a1568f1c5d27d905d3d4950c725b13e2c8dd4bd
Mustaque Khan <130723242+mustaque789@users.noreply.github.com>
2024-02-01 03:00:54 -0500

    navigation & carousel

---
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import MainCarousel from '../../components/HomeCarousel/MainCarousel'
+
+const HomePage = () => {
+  return (
+    <div>
+        <MainCarousel/>
+
+        <div>
+            Other Sections
+        </div>
+      
+    </div>
+  )
+}
+
+export default HomePage
