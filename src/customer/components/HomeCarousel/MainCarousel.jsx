8e075ae5aee27dda115c2af99d6ecdba7e580079
Mustaque Khan <130723242+mustaque789@users.noreply.github.com>
2024-02-11 21:48:58 -0500

    Update MainCarousel.jsx

--- src/customer/components/HomeCarousel/MainCarousel.jsx
+++ src/customer/components/HomeCarousel/MainCarousel.jsx
@@ -5,14 +5,14 @@ import { mainCarouselData } from './MainCarouselData';
 
 
 const MainCarousel = () => {
-// iterating main carousel
+// iterating main carousel 
   const items= mainCarouselData.map((item)=> <img className='cursor-pointer'   // iterating images as loop and putting inside <img src=""
      role='presentation'
      src={item.image}
      alt='' />
     )
 
-
+// passing into alice carousel
   return (
     <AliceCarousel
         mouseTracking
